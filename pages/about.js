e50dd956bfd8df5611c82f71f1596df8a5072dfa
acamacho88 <billyshears06@gmail.com>
2019-05-26 13:17:52 -0700

    made modification to biography

--- pages/about.js
+++ pages/about.js
@@ -13,14 +13,14 @@ const About = () => (
             <div className="title">Bio</div>
             <div className="aboutText">
                 <img className="bioPic" src="/static/alex-guitar.png" />
+                <p>I have worked at a tech startup the last five years and made use of existing web development knowledge I already had and greatly expanded it during this time.</p>
+
                 <p>In my current job and my side projects, I take great enjoyment in finding new ways to solve problems, and approach issues from a different angle.
                     I'm always excited to learn about new technologies, fill gaps in my existing knowledge, and pass the information I've gained onto team members knowing that as all of us grow stronger, the team and product get stronger as well.
                 </p>
 
                 <p>I graduated from the Massachusetts Institute of Technology with a Bachelors of Science in Physics, and had great fun with the marriage of hard, empirical data along with analytical/programmatic ways of analyzing them, and seeing how they fit into broader theories.</p>
                 
-                <p>I have worked at a tech startup the last five years and made use of existing web development knowledge I already had and greatly expanded it during this time.</p>
-
                 <p>In addition, during my time over the last five years, I've enrolled in courses to go through official fullstack learning materials, including Udacity Fullstack Nanodegree and UC Berkeley Extension fullstack bootcamp.
                      I learned a great deal working on the assignments and building applications in group projects where we would develop a concept of the site, plan out the responsibilities, and launch the app in two weeks' time.</p>
 
